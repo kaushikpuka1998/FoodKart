997555a5ccac198ccb445e9146fbb19ec823b04f
kaushikpuka1998 <52675676+kaushikpuka1998@users.noreply.github.com>
2024-10-22 01:47:52 +0530

    DAta Lombok added for reducing the code

--- src/main/java/com/example/foodkart/ENTITY/Restaurent.java
+++ src/main/java/com/example/foodkart/ENTITY/Restaurent.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Restaurent {
     private String name;
     private List<String> serviceablePincode;
@@ -15,71 +18,4 @@ public class Restaurent {
     private double price;
     private double rating;
     private int ratingCount;
-
-    public Restaurent(String name, List<String> serviceablePincode, String foodItemName, int quantity, double price, double rating, int ratingCount) {
-        this.name = name;
-        this.serviceablePincode = serviceablePincode;
-        this.foodItemName = foodItemName;
-        this.quantity = quantity;
-        this.price = price;
-        this.rating = rating;
-        this.ratingCount = ratingCount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<String> getServiceablePincode() {
-        return serviceablePincode;
-    }
-
-    public void setServiceablePincode(List<String> serviceablePincode) {
-        this.serviceablePincode = serviceablePincode;
-    }
-
-    public String getFoodItemName() {
-        return foodItemName;
-    }
-
-    public void setFoodItemName(String foodItemName) {
-        this.foodItemName = foodItemName;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public int getRatingCount() {
-        return ratingCount;
-    }
-
-    public void setRatingCount(int ratingCount) {
-        this.ratingCount = ratingCount;
-    }
-
 }
